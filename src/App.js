a23aae62c206be23f2b14a43e11d4761c84696eb
Fitra Muhammad Anugrah <121858668+Samunee@users.noreply.github.com>
2024-01-19 20:14:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import FriendList from "./components/FriendList";
 const initialFriends = [
   {
     id: 118836,
-    name: "Budi",
+    name: "Fitra",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sukma",
+    name: "Sung Jin Wo",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Parjo",
+    name: "BJ",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
